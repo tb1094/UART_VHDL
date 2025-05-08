6939469a0ace0003ace318a2c39fbfd1ab05bec3
tb1094 <tb1094@tuta.io>
2025-05-08 10:45:53 +0200

    updated linux_user_apps/gameselection.c with popup window for aop level selection

--- linux_user_apps/gameselection.c
+++ linux_user_apps/gameselection.c
@@ -9,6 +9,22 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
 void nc_setup();
+int popup(const char*);
+
+const char *aop_levels[] = {
+  NULL,
+  "aop-level-01.txt",
+  "aop-level-02.txt",
+  "aop-level-03.txt",
+  "aop-level-04.txt",
+  "aop-level-05.txt",
+  "aop-level-06.txt",
+  "aop-level-07.txt",
+  "aop-level-08.txt",
+  "aop-level-09.txt",
+  "aop-level-10.txt",
+  "aop-level-11.txt"
+};
 
 const char *choices[] = {
   "Minesweeper",
@@ -18,7 +34,8 @@ const char *choices[] = {
   "Pong",
   "Ambassador of Pain",
   "Galaxis",
-  "Mogria's Snake"
+  "Mogria's Snake",
+  "Tic Tac 4"
 };
 
 int main() {
@@ -69,6 +86,8 @@ int main() {
         choice = highlight;
         break;
     }
+
+    int value;
 
     switch (choice) {
       case 0:
@@ -151,6 +170,12 @@ int main() {
         }
         break;
       case 5:
+        c = popup("Enter number to select level: 1-9");
+        value = c - '0';
+        if (value < 1 || value > 11) {
+          clear();
+          break;
+        }
         endwin();
         pid = fork();
         if (pid == 0) { // child process
@@ -158,7 +183,7 @@ int main() {
             perror("chdir failed for aop");
             return 1;
           }
-          execl("./aop", "./aop", "aop-level-03.txt", NULL);
+          execl("./aop", "./aop", aop_levels[value], NULL);
           perror("execl failed for aop");
           _exit(EXIT_FAILURE);
         } else if (pid > 0) { // parent process
@@ -199,6 +224,21 @@ int main() {
           refresh();
         }
         break;
+      case 8:
+        endwin();
+        pid = fork();
+        if (pid == 0) { // child process
+          execl("./tictac4-1.0/tictac4", "./tictac4-1.0/tictac4", NULL);
+          perror("execl failed for tictac4");
+          _exit(EXIT_FAILURE);
+        } else if (pid > 0) { // parent process
+          int status;
+          waitpid(pid, &status, 0);
+          nc_setup();
+          clear();
+          refresh();
+        }
+        break;
     }
     choice = -1;
   }
@@ -217,4 +257,22 @@ void nc_setup() {
   if (ret == ERR) {
     mvprintw(1, 0, "Warning: curs_set(0) failed.");
   }
+}
+
+int popup(const char *message) {
+  int width = strlen(message) + 4;
+  int height = 5;
+  int starty = (LINES - height) / 2;
+  int startx = (COLS - width) / 2;
+  int c;
+
+  WINDOW *popup_win = newwin(height, width, starty, startx);
+  box(popup_win, 0, 0);
+
+  mvwprintw(popup_win, 2, 2, "%s", message);
+  wrefresh(popup_win);
+
+  c = wgetch(popup_win);  // wait for user input
+  delwin(popup_win);
+  return c;
 }
